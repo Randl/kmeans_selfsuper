b24bcb632152ae402b4932fe703629fcd4a87dbf
Evgenii Zheltonozhskii <zheltonozhskiy@gmail.com>
2020-06-24 12:08:23 +0300

    Rewrite accuracy to be linear in sample count. Transform PCA in batches in-place.

--- cluster.py
+++ cluster.py
@@ -1,3 +1,4 @@
+import gc
 import os
 import time
 
@@ -12,27 +13,20 @@ from sklearn.utils import shuffle
 from tqdm import trange, tqdm
 
 n_classes = 1000
-n_clusters = 2 * n_classes  # https://arxiv.org/abs/2005.12320 -- overclustering
+n_clusters = 1 * n_classes  # https://arxiv.org/abs/2005.12320 -- overclustering
 train_size = 12811  # 67
 val_size = 500  # 00
 
 epochs = 60
 
 n_features = 2048
-batch_size = 2048
+batch_size = max(2048, int(2 ** np.ceil(np.log2(n_clusters))))
 
 
 def get_cost_matrix(y_pred, y):
-    # TODO test
-    C1 = np.zeros((n_clusters, y_pred.size))
-    C1[y_pred, np.arange(y_pred.size)] = 1
-    if len(y.shape) == 1:
-        C2 = np.zeros((y.size, n_classes))
-        C2[np.arange(y.size), y] = 1
-    else:
-        C2 = y.astype(int)
-
-    C = np.matmul(C1.astype(int), C2.astype(int))  # num_clusters * num_classes
+    C = np.zeros((n_clusters, n_classes))
+    for pred, label in zip(y_pred, y):
+        C[pred, label] += 1
     return C
 
 
@@ -69,7 +63,9 @@ def batches(l, n):
         yield l[i:i + n]
 
 
-def print_metrics(y_pred, y_true, train_lin_assignment, train_maj_assignment, val_lin_assignment, val_maj_assignment):
+def print_metrics(y_pred, y_true,
+                  train_lin_assignment, train_maj_assignment,
+                  val_lin_assignment, val_maj_assignment):
     C = get_cost_matrix(y_pred, y_true)
 
     acc_tr_lin = accuracy_from_assignment(C, *train_lin_assignment, len(y_true))
@@ -84,22 +80,20 @@ def print_metrics(y_pred, y_true, train_lin_assignment, train_maj_assignment, va
 
     print("ARI {:.4f}\tV {:.4f}\tAMI {:.4f}\tFM {:.4f}\n".format(ari, v_measure, ami, fm))
     print("ACC TR L {:.4f}\tACC TR M {:.4f}\t"
-          "ACC VA L {:.4f}\tACC VA M {:.4f}\n".format(acc_tr_lin, acc_tr_maj, acc_va_lin, acc_va_maj))
+          "ACC VA L {:.4f}\tACC VA M {:.4f}\n".format(acc_tr_lin, acc_tr_maj,
+                                                      acc_va_lin, acc_va_maj))
 
 
-def train_pca(n_components=128):
-    transformer = IncrementalPCA(n_components=n_components, batch_size=4096)  #
-    for i, batch in enumerate(tqdm(batches(X_train, 4096), total=len(X_train) // 4096)):
+def train_pca(X_train):
+    bs = max(4096, X_train.shape[1] * 2)
+    transformer = IncrementalPCA(batch_size=bs)  #
+    for i, batch in enumerate(tqdm(batches(X_train, bs), total=len(X_train) // bs)):
         transformer = transformer.partial_fit(batch)
     print(transformer.explained_variance_ratio_.cumsum())
     return transformer
 
 
 def cluster_data(X_train, y_train, X_test, y_test):
-    mean, std = X_train.mean(0), X_train.std()
-    X_train = (X_train - mean) / std
-    X_test = (X_test - mean) / std
-
     minib_k_means = cluster.MiniBatchKMeans(n_clusters=n_clusters, batch_size=batch_size, max_no_improvement=None)
 
     for e in trange(epochs):
@@ -108,13 +102,21 @@ def cluster_data(X_train, y_train, X_test, y_test):
             minib_k_means = minib_k_means.partial_fit(batch)
 
         pred = minib_k_means.predict(X_train)
-        C_train = get_cost_matrix(pred, y_test)
+        C_train = get_cost_matrix(pred, y_train)
 
         y_pred = minib_k_means.predict(X_test)
         C_val = get_cost_matrix(y_pred, y_test)
 
         print_metrics(y_pred, y_test, assign_classes_hungarian(C_train), assign_classes_majority(C_train),
                       assign_classes_hungarian(C_val), assign_classes_majority(C_val))
+
+
+def transform_pca(X, transformer):
+    n = max(4096, X.shape[1] * 2)
+    for i in trange(0, len(X), n):
+        X[i:i + n] = transformer.transform(X[i:i + n])
+        break
+    return X
 
 
 generate = False
@@ -139,13 +141,26 @@ else:
         t1 = time.time()
 
         print('Loading time: {:.4f}'.format(t1 - t0))
-        transformer = train_pca(n_components=128)
-        X_train, X_test = transformer.transform(X_train), transformer.transform(X_test)
+
+        y_train, y_test = y_train.argmax(1), y_test.argmax(1)
+        transformer = train_pca(X_train)
+        X_train, X_test = transform_pca(X_train, transformer), transform_pca(X_test, transformer)
+        gc.collect()
         np.savez(filename, train_embs=X_train, train_labs=y_train, val_embs=X_test, val_labs=y_test)
     else:
+        t0 = time.time()
         data = np.load(filename)
         X_train, y_train, X_test, y_test = data['train_embs'], data['train_labs'], data['val_embs'], data['val_labs']
-    cluster_data(X_train, y_train, X_test, y_test)
+        if len(y_train.shape) > 1:
+            y_train, y_test = y_train.argmax(1), y_test.argmax(1)
+        t1 = time.time()
+        print('Loading time: {:.4f}'.format(t1 - t0))
+        X_train, X_test = X_train[:, :n_components], X_test[:, :n_components]
+        cluster_data(X_train, y_train, X_test, y_test)
 
+# ResNet50
 # k-means: 0.0370
 # PCA+k-means: 0.223
+# over (2): ARI 0.0936	V 0.6336	AMI 0.2520	FM 0.0969
+#
+# ACC TR L 0.2293	ACC TR M 0.2386	ACC VA L 0.2714	ACC VA M 0.2774
